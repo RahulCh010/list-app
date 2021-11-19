1b6f090b3ead571e9e8e568ff4a42365a279b028
Rahul Choudhary <rahul-choudhary@hcl.com>
2021-11-19 23:27:06 +0530

    Added dynamic class for form-control

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,22 +5,35 @@ import "./CourseInput.css";
 
 const CourseInput = (props) => {
 	const [enteredValue, setEnteredValue] = useState("");
+	const [isValid, setIsValid] = useState(true);
 
 	const goalInputHandler = (event) => {
+		if (event.target.value.trim() !== "") {
+			setIsValid(true);
+		}
 		setEnteredValue(event.target.value);
 	};
 
 	const formSubmitHandler = (event) => {
 		event.preventDefault();
-		props.onAddGoal(enteredValue);
+		if (enteredValue.trim() === "") {
+			setIsValid(false);
+			return;
+		}
+		props.onAddGoal(enteredValue.trim());
 		setEnteredValue("");
 	};
 
 	return (
 		<form onSubmit={formSubmitHandler}>
-			<div className="form-control">
-				<label>Course Goal</label>
-				<input type="text" value={enteredValue} onChange={goalInputHandler} />
+			<div className={`form-control ${isValid ? "" : "invalid"}`}>
+				<label style={{ color: isValid ? "black" : "red" }}>Course Goal</label>
+				<input
+					style={{ borderColor: isValid ? "#fad0ec" : "red" }}
+					type="text"
+					value={enteredValue}
+					onChange={goalInputHandler}
+				/>
 			</div>
 			<Button type="submit">Add Goal</Button>
 		</form>
